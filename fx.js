901c8318f9731d7c4dadb62ed772b74d5abf2722
khnoh <noh5524@gmail.com>
2020-10-03 20:53:23 +0900

    feat: 동기/비동기 상황 에러 핸들링

--- fx.js
+++ fx.js
@@ -6,6 +6,7 @@ const go = (...args) => reduce((a, f) => f(a), args);
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 const sum = curry((fn, iter) => go(iter, map(fn), reduce(add)));
 const goPromise = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
+// "Symbol 값도 객체의 프로퍼티 키로 사용할 수 있다. Symbol 값은 유일한 값이므로 Symbol 값을 키로 갖는 프로퍼티는 다른 어떠한 프로퍼티와도 충돌하지 않는다."
 const nop = Symbol("nop");
 const L = {};
 
@@ -21,9 +22,8 @@ const head = iter => goPromise(take(1, iter), ([h]) => h);
 
 const reduce = curry((f, acc, iter) => {
   if (!iter) return reduce(f, head((iter = acc[Symbol.iterator]())), iter);
-  else {
-    iter = iter[Symbol.iterator]();
-  }
+
+  iter = iter[Symbol.iterator]();
 
   // 재귀를 이용해 promise 다음 함수가 promise가 아닐경우 하나의 콜스택에서 전부 실행되도록 한다.
   return goPromise(acc, function recur(acc) {
@@ -59,7 +59,7 @@ const take = curry((l, iter) => {
       if (a instanceof Promise)
         return a
           .then(a => ((res.push(a), res).length === l ? res : recur()))
-          .catch(e => (e === nop ? recur() : Promise.reject()));
+          .catch(e => (e === nop ? recur() : Promise.reject(e)));
       if ((res.push(a), res).length === l) return res;
     }
 
